619b8c5f82da7a879678a74e1eeab33781cb4d7e
megi010 <ms01010@student.uni-lj.si>
2025-03-28 17:11:08 +0100

    changes

--- evropa.py
+++ evropa.py
@@ -1,6 +1,13 @@
 import koordinate_iata as coord_iata
-import requests
+import requests, json, os
 
 evropa_iata = ["LHR", "IST", "CDG", "AMS", "MAD", "FRA", "BCN", "FCO", "LGW", "SVO", "MUC", "LIS", "DUB", "ATH", "VIE", "ZRH", "MXP", "OSL", "BRU", "WAW", "BUD", "ARN", "VCE", "NCE", "OTP", "HEL", "BEG", "KEF"]
 
 # Shranjevanje
+#Pridobivanje podatkov 
+params = {"access_key": "62905fc234eca138b48941106dcc9e98", 
+             "dep_iata": evropa_iata}
+
+res = requests.get("https://api.aviationstack.com/v1/flights", params=params)
+
+print(res.json())
