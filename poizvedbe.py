41e5ffc314aad01d8026e5b91a8582b283e11467
Vid Gobec <91780369+VidGobec@users.noreply.github.com>
2025-03-31 19:02:42 +0200

    spremenil ime get_podatki_letov in ga spremenil v class
    naredil sem class za poizvedovanje podatkov iz naših gnezndenih slovarjev podatkov.

---
+++ poizvedbe.py
@@ -0,0 +1,86 @@
+
+
+
+class poizvedbe():
+    def __init__(self, json):
+          self._data = json
+          pass
+     
+    def get_data(self):
+         return self._data
+
+    @staticmethod
+    def _get_element(iskan, slovar):
+        """prejme tabelo nizov iskan in slovar in vrne vrednost vrednosti shranjeno pod kljucem iskan.
+        tabela kljucov je sestavljena iz kljucev gnezdenih slovarjev, kjer je prvi element tabele najbolj zunanji kljuc slovarja
+        """
+        if len(iskan)>1:
+             return poizvedbe._get_element(iskan[1:], slovar[iskan[0]])
+        return slovar[iskan[0]]
+            
+              
+
+    def get_grouped_data(self, sortiran_po):
+        """
+        prejme tabelo nizov kljucev in vrne slovar kjer so kljuc grupirani po zadnjem elementu v prijeti tabeli vrednosti pa so podatki, ki imajo isto vrednost, po katerem grupiramo
+        primer get_grouped_data(["flight_status"]) vrne slovar kjer so kljuci vrednosti data["flight_status"] vrednosti je tabela slovarjev, ki imajo enak flight status
+        """
+        slovar = dict()
+        for element in self.get_data():
+            kljuc = poizvedbe._get_element(sortiran_po, element)
+            if kljuc == None:
+                Kljuc = "None"
+            if kljuc in slovar:
+                slovar[kljuc].append(element)
+            else:
+                slovar[kljuc] = [element]
+
+        return slovar
+                                
+    @staticmethod
+    def get_pristali(json):
+        """prejme json z podatki letov, in vrne vse lete, ki so že pristali"""
+        return [x for x in json if x["flight_status"]=="landed" ]
+    @staticmethod
+    def get_zamudniki(json):
+        """prejme json z podatki letov, in vrne vse lete, ki so imeli zamudo ob pristanku in odhodu"""
+        return [x for x in json if x["arrival"]["delay"] is not None and x["arrival"]["delay"]>0 and x["departure"]["delay"] is not None and x["departure"]["delay"]>0]
+    @staticmethod
+    def get_zamudniki_ob_pristaneku(json):
+            """prejme json z podatki letov, in vrne vse lete, ki so imeli zamudo ob pristanku"""
+            return [x for x in json if x["arrival"]["delay"] is not None and x["arrival"]["delay"]>0]
+    @staticmethod
+    def get_zamudniki_ob_odhodu(json):
+            """prejme json z podatki letov, in vrne vse lete, ki so imeli zamudo ob odhodu"""
+            return [x for x in json if x["departure"]["delay"] is not None and x["departure"]["delay"]>0]
+    
+
+         
+
+    @staticmethod
+    def get_vrednosti(json, tab_kljucev):
+        """prejme json z podatki letov in tabela kljucev gnezdenega slovarja, kjer je prvi element najbolj zunanji kljuc.
+        Vrne tabelo elementov, kamor kazejo vneseni kljuci
+        """
+        #try:
+        return [poizvedbe._get_element(tab_kljucev, x) for x in json]
+        #except:
+            #return None
+
+
+
+import matplotlib as plt
+
+
+ax.scatter(delta1[:-1], delta1[1:], c=close, s=volume, alpha=0.5)
+
+ax.set_xlabel(r'$\Delta_i$', fontsize=15)
+ax.set_ylabel(r'$\Delta_{i+1}$', fontsize=15)
+ax.set_title('Volume and percent change')
+
+ax.grid(True)
+fig.tight_layout()
+
+plt.show()
+
+
---
+++ poizvedbe_testi.py
@@ -0,0 +1,41 @@
+from poizvedbe import poizvedbe
+import json
+
+with open('podatki_svet.json', 'r') as file:
+    data = json.load(file)
+
+
+def povp_vrednost(tabela):
+    """vrne povprecno vrednost stevil v tabeli"""
+    return sum(x for x in tabela if x is not None)/len(tabela)
+
+##tests
+pristali = poizvedbe.get_pristali(data)
+#print(pristali[0],"\n", pristali[1])
+zamudniki = poizvedbe.get_zamudniki(pristali)
+#print(zamudniki[0])
+print("ce gledamo vse lete v podatki_svet")
+zamudniki_ob_pristanku = poizvedbe.get_zamudniki_ob_pristaneku(data)
+povp = povp_vrednost([x["arrival"]["delay"] for x in zamudniki_ob_pristanku])
+print(f"povp vrednost zamude letal v odhodu je {povp}")
+
+zamudniki_ob_odhodu = poizvedbe.get_zamudniki_ob_odhodu(data)
+povp = povp_vrednost([x["departure"]["delay"] for x in zamudniki_ob_odhodu])
+print(f"povp vrednost zamude letal v prihodu je {povp}")
+
+##
+print("\nce gledamo le pristale lete v podatki_svet\n")
+print("ce gledamo vse lete v podatki_svet")
+zamudniki_ob_pristanku = poizvedbe.get_zamudniki_ob_pristaneku(pristali)
+povp = povp_vrednost([x["arrival"]["delay"] for x in zamudniki_ob_pristanku])
+print(f"povp vrednost zamude letal v odhodu je {povp}")
+
+zamudniki_ob_odhodu = poizvedbe.get_zamudniki_ob_odhodu(pristali)
+povp = povp_vrednost([x["departure"]["delay"] for x in zamudniki_ob_odhodu])
+print(f"povp vrednost zamude letal v prihodu je {povp}")
+
+##
+print("############################")
+x = poizvedbe.get_vrednosti(pristali, ["departure","delay"])
+povp = povp_vrednost(x)
+print(f"povp vrednost zamude letal v prihodu je {povp}")
